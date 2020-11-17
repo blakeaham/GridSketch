ee711d5a249ccea297d75e757fcf95e009ff048b
blakeaham <blakeaham@gmail.com>
2020-11-16 23:45:30 -0600

    updating
    making some stuff

--- gridsketch.js
+++ gridsketch.js
@@ -1,12 +1,50 @@
 const bigGrid = document.querySelector(".bigContainer");
+const canvasheight = bigGrid.offsetHeight - 20;
+const clearBtn = document.querySelector("#clearBtn");
+let power = false;
+clearBtn.addEventListener('click', clearGrid);
 
 function makeGrid(num) {
-
-    for (i=0; i<num; i++){
+    let gridPixels = bigGrid.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove())
+    for (i=0; i<num*num; i++){
+        
+        bigGrid.style.gridTemplateColumns = `repeat(${num}, ${Math.floor(canvasheight/num)}px)`;
+        bigGrid.style.gridTemplateRows = `repeat(${num}, ${Math.floor(canvasheight/num)}px)`;
         let newDiv = document.createElement("div");
         newDiv.classList.add("sketchGridDiv");
         bigGrid.appendChild(newDiv);
     }
+    gridPixels = bigGrid.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid))
 }
 
-makeGrid(5);
+function clearGrid() {
+    console.log('clearing');
+    let gridPixels  = bigGrid.querySelectorAll('div');
+    gridPixels.forEach(gp => gp.style.backgroundColor = "white");
+}
+
+function colorGrid(e) {
+    // console.log(e.target)
+    if(power) {
+    e.target.style.backgroundColor = "red";
+    }
+}
+
+window.onclick = function(event) {
+    power = !power;
+}
+
+// makeGrid(5);
+
+const modal = document.querySelector(".modal");
+
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     } else {
+//       modal.style.display = "block";
+//     }
+//   }
